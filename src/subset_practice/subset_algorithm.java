070d84989a19be48ce321a1a5fa186785c0cd8c9
alin2202 <alina.lotfulina@gmail.com>
2015-09-30 19:07:04 -0700

    Practice on subset and ArrayList implementation

--- src/subset_practice/subset_algorithm.java
+++ src/subset_practice/subset_algorithm.java
@@ -5,55 +5,56 @@ import java.util.stream.IntStream;
 
 public class subset_algorithm {
 
-	public ArrayList<Integer> getSubset(int[] set, int target){
-		ArrayList<Integer> subset = new ArrayList<Integer>();
-		subset.add(0);
-		// implementation
-		// outer loop: through all ints in set:
-		for (int set_i = 0; set_i < set.length; set_i++){
-			for (int subset_i = 0; subset_i < subset.size(); subset_i ++){
-				// get sum of subset items
-				int sum = getArrayListSum(subset);
-				int current_i = set[set_i];
-				// if sum is less than target, add one more element
-				if ((sum + current_i) < target){
-					subset.add(current_i);
-					// DEBUG STATEMENT
-					System.out.println(subset);
+	/**
+	 * Returns closest to a target subset of integers
+	 * @param set
+	 * @param target
+	 * @return ArrayList<Integer> result_subset
+	 */
+	public ArrayList<Integer> getSubset(int[] set, int target){	
+		// create array list to hold the result
+		ArrayList<Integer> new_subset = null;	
+		// create array list to hold all sub-sets
+		ArrayList<ArrayList<Integer>> subsets_list = new ArrayList<ArrayList<Integer>>();
+		subsets_list.add(new ArrayList<Integer>(0));
+		// loop through all ints in set:
+		for (int i_set = 0; i_set < set.length; i_set++){
+			ArrayList<ArrayList<Integer>> temp_list = new ArrayList<ArrayList<Integer>>();
+			// loop through all subsets of subsets_list
+			for (int i_subset = 0; i_subset < subsets_list.size(); i_subset ++){
+				ArrayList<Integer> current_subset = subsets_list.get(i_subset);
+				// calculate sum of current subset
+				int sum = getArrayListSum(current_subset);
+				// get current element of initial set
+				int current_i = set[i_set];
+				// if sum of current subset and current element of the set
+				// is less than target, add one more element
+				if ((sum + current_i) <= target){
+					// create new subset and add current element of the set to a new subset
+					new_subset = new ArrayList<Integer>();
+					new_subset.addAll(current_subset);
+					// add current element of the set to a new subset
+					new_subset.add(current_i);
+					// add new subset to our list of subsets
+					temp_list.add(new_subset);
 				}
 				// if sum is equal to target, break out of loops
 				if ((sum + current_i) == target){
-					return subset;
+					return new_subset;
 				}
 			}
+			subsets_list.addAll(temp_list);
 		}
 		
-		/*
-		 * Col = {}
-		1) 	// outer loop: 0 element (5)
-		0.1 inner loop: Col(0) = {}
-		if sum({}) + 5 < 37 == true: Col = {}, {5}
-		is sum({}) + 5 == 37: false
-
-		Col = {}, {5}
-		2)  //outer loop: 1 element (7)
-		1.1 inner loop: Col(0) = {0}
-		if sum(0) + 7 < 37 == true: Col = {}, {7}
-		if sum(0) + 7 == 37: false
-		
-		1.2 inner loop" Col(1) = {5}
-		if sum(5) + 7 < 37 == true: Col = {}, {5}, {7}
-		if sum(5) + 7 == 37: false
-		 */
-		
-		return subset;		
+		return null;
 	}
 	
 	public int getArrayListSum(ArrayList<Integer> list){
 		int sum = 0;
 		for (int i = 0; i < list.size(); i++){
-			sum = sum + list.get(i);
+			int listElement = list.get(i);
+			sum = sum + listElement;
 		}
-		return 0;
+		return sum;
 	}
 }
--- src/subset_practice/subset_practice.java
+++ src/subset_practice/subset_practice.java
@@ -5,14 +5,21 @@ import java.util.ArrayList;
 public class subset_practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] set = {5, 7, 13, 14, 3, 17, 9, 3, 11, 20};
-		int target = 37;
-		subset_algorithm subsetobj = new subset_algorithm();
-		ArrayList<Integer> subset = subsetobj.getSubset(set, target);
+		int[] intSet = {5, 7, 1, 2};
+		// int[] set = {5, 7, 13, 14, 3, 17, 9, 3, 11, 20};
 		
+//		ArrayList<Integer> arraySet = new ArrayList<Integer>();
+//		for (int i = 0; i < intSet.length; i++){
+//			arraySet.add(intSet[i]);
+//		}
+		
+		int target = 16;
+		
+		subset_algorithm subsetobj = new subset_algorithm();
+//		//DEBUG: checking sum function
+//		int sum = subsetobj.getArrayListSum(arraySet);
+		
+		ArrayList<Integer> subset = subsetobj.getSubset(intSet, target);
 		System.out.println(subset);
 	}
-
-	
 }
